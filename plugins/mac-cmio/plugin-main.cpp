8a8c40dec0a08dc6db400a36f6c8e88f3dbb7afa
Andrew Lin <andrew.lin@snapchat.com>
2020-03-31 10:41:47 -0400

    nv12 to uyvy conversion function

--- plugins/mac-cmio/plugin-main.cpp
+++ plugins/mac-cmio/plugin-main.cpp
@@ -18,6 +18,53 @@ obs_output_t *virtual_out;
 bool output_running = false;
 bool audio_running = false;
 
+static void nv12_to_y422(uint8_t *in, int width, int height, uint8_t **out,
+			 int *out_length)
+{
+	*out_length = width * height * 2;
+	blog(LOG_INFO, "out_length: %d", *out_length);
+
+	uint8_t *frame = (uint8_t *)bzalloc(*out_length);
+	blog(LOG_INFO, "out bzalloc: %p", *frame);
+
+	uint8_t *y = in;
+	uint8_t *uv = y + (width * height);
+	for (int r = 0; r < height; r += 1) {
+		for (int c = 0; c < width; c += 2) {
+			int i = r * width + c;
+			int uv_i = ((r / 2) * width) + c;
+			// U0
+			frame[i * 2] = (uv[uv_i] & 0x01) +
+				       ((uv[uv_i] >> 1) & 0x02) +
+				       ((uv[uv_i] >> 2) & 0x04) +
+				       ((uv[uv_i] >> 3) & 0x08) +
+				       ((uv[uv_i + 1] << 4) & 0x10) +
+				       ((uv[uv_i + 1] << 3) & 0x20) +
+				       ((uv[uv_i + 1] << 2) & 0x40) +
+				       ((uv[uv_i + 1] << 1) & 0x80);
+			// V0
+			frame[i * 2 + 2] = ((uv[uv_i] >> 1) & 0x01) +
+					   ((uv[uv_i] >> 2) & 0x02) +
+					   ((uv[uv_i] >> 3) & 0x04) +
+					   ((uv[uv_i] >> 4) & 0x08) +
+					   ((uv[uv_i + 1] << 3) & 0x10) +
+					   ((uv[uv_i + 1] << 2) & 0x20) +
+					   ((uv[uv_i + 1] << 1) & 0x40) +
+					   (uv[uv_i + 1] & 0x80);
+
+			frame[i * 2] = uv[uv_i];
+			frame[i * 2 + 2] = uv[uv_i + 1];
+
+			// Y0
+			frame[i * 2 + 1] = y[i];
+			// Y1
+			frame[i * 2 + 3] = y[i + 1];
+		}
+	}
+
+	*out = frame;
+}
+
 static void virtual_output_destroy(void *data)
 {
 	output_running = false;
@@ -48,6 +95,10 @@ static const char *cmio_output_getname(void *unused)
 static void cmio_output_stop(void *data, uint64_t ts)
 {
 	blog(LOG_INFO, "%s", "CMIO stop");
+	virtual_out_data *out_data = (virtual_out_data *)data;
+	obs_output_end_data_capture(out_data->output);
+	output_running = false;
+	audio_running = false;
 }
 
 static void cmio_output_destroy(void *data)
@@ -57,7 +108,26 @@ static void cmio_output_destroy(void *data)
 
 static void *cmio_output_create(obs_data_t *settings, obs_output_t *output)
 {
+	/*
+	FILE *test_file =
+		fopen("/Users/andrewlin/Downloads/test_nv12.yuv", "r");
+	uint8_t *frame = new uint8_t[1382400];
+	fread(frame, 1, 1382400, test_file);
+
+	uint8_t *converted;
+	int converted_length;
+	nv12_to_y422(frame, 1280, 720, &converted, &converted_length);
 	blog(LOG_INFO, "%s", "CMIO create");
+
+	FILE *fp;
+	fp = fopen("/Users/andrewlin/Downloads/converted_nv12_to_y422.yuv",
+		   "w");
+	blog(LOG_INFO, "CMIO raw_video - fp %lld", fp);
+	fwrite(converted, 1, converted_length, fp);
+	fclose(fp);
+
+	exit(0);
+	*/
 
 	virtual_out_data *data =
 		(virtual_out_data *)bzalloc(sizeof(struct virtual_out_data));
@@ -84,6 +154,8 @@ static bool cmio_output_start(void *data)
 
 	out_data->width = (int32_t)obs_output_get_width(out_data->output);
 	out_data->height = (int32_t)obs_output_get_height(out_data->output);
+	blog(LOG_INFO, "CMIO start: Video Size - %d x %d", out_data->width,
+	     out_data->height);
 
 	if (out_data) {
 		obs_output_begin_data_capture(out_data->output, 0);
@@ -103,9 +175,33 @@ static obs_properties_t *cmio_output_properties(void *unused)
 	return props;
 }
 
+bool wrote_frame = false;
+
 static void cmio_output_raw_video(void *param, struct video_data *frame)
 {
-	blog(LOG_INFO, "%s", "CMIO raw_video");
+	blog(LOG_INFO, "CMIO raw_video - timestamp %lld", frame->timestamp);
+	if (!wrote_frame) {
+		for (int i = 0; i < MAX_AV_PLANES; i++) {
+			blog(LOG_INFO, "frame %d linesize %d", i,
+			     frame->linesize[i]);
+		}
+
+		uint8_t *converted;
+		int converted_length;
+		nv12_to_y422(frame->data[0], 1280, 720, &converted,
+			     &converted_length);
+
+		FILE *fp;
+		fp = fopen(
+			"/Users/andrewlin/Downloads/converted_nv12_to_y422.yuv",
+			"w");
+		blog(LOG_INFO, "CMIO raw_video - fp %lld", fp);
+		fwrite(converted, 1, converted_length, fp);
+		fclose(fp);
+
+		bfree(converted);
+		wrote_frame = true;
+	}
 }
 
 static void cmio_output_raw_audio(void *data, struct audio_data *frames)
